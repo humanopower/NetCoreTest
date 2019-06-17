4616d69b18c76525c5f6db45f132e28416330a41
yamil <yamil.israel.ortega@pemex.com>
2019-06-17 16:14:16 -0500

    NAda

--- TesterDemo/PCatalogosInstitucionales/Controllers/ValuesController.cs
+++ TesterDemo/PCatalogosInstitucionales/Controllers/ValuesController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
+//Yes, another comment
 namespace PCatalogosInstitucionales.Controllers
 {
 	[Route("api/[controller]")]
